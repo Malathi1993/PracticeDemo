77adce854b39a08165a50621461550f784d06e2f
Malathi <57785954+Malathi1993@users.noreply.github.com>
2020-06-04 15:53:04 +0530

    ad

--- app/src/main/java/com/example/practicedemo/RegistrationActivity.java
+++ app/src/main/java/com/example/practicedemo/RegistrationActivity.java
@@ -8,11 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class RegistrationActivity extends AppCompatActivity {
     EditText Name; EditText Surname; EditText Email; EditText phoneNumber; EditText PostalAddress;
     EditText Place; EditText Gender; EditText FotherName; EditText MotherName; TextView ifAlready;
-    Button Register;
+    Button Register; Toolbar Registration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +29,7 @@ public class RegistrationActivity extends AppCompatActivity {
         MotherName=findViewById(R.id.editText10);
         ifAlready=findViewById(R.id.textView);
         Register=findViewById(R.id.button);
+       // Registration=findViewById(R.id.Registration);
 
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +46,12 @@ public class RegistrationActivity extends AppCompatActivity {
                 Intent it=new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(it);
             }
+           // TextView mTitle = (TextView) Toolbar.findViewById(R.id.Registration);
+
+           // setSupportActionBar(Toolbar);
+           // mTitle.setText(Toolbar.getTitle());
+
+           // getSupportActionBar().setDisplayShowTitleEnabled(false);
         });
     }
 }
